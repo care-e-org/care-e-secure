76d26a0c185e6f29c96bb768037b4f1a8619a1c7
Surya <suryacsbs@gmail.com>
2026-03-07 18:37:30 +0530

    Force DB creation on app load

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
-from app import create_app
+import os
+from app import create_app, db
+from app.models import AdminUser
+from werkzeug.security import generate_password_hash
 
 app = create_app()
 
+# This forces the database to build the moment Gunicorn loads the app
+with app.app_context():
+    db.create_all()
+    
+    # Check if your admin account exists, if not, create it
+    if not AdminUser.query.filter_by(username='suryarkd').first():
+        admin_pass = os.environ.get('SECRET_KEY')
+        if admin_pass:
+            admin = AdminUser(username='suryarkd', password_hash=generate_password_hash(admin_pass))
+            db.session.add(admin)
+            db.session.commit()
+
 if __name__ == '__main__':
-    # Running on 0.0.0.0 so you can potentially view it from another device on your local network
-    app.run(host='127.0.0.1', port=5000)
+    app.run(debug=True)
